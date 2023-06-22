855813b3be9cce3d3f0c4603d764a337b183ce2f
michal.dzierzbicki <michal.dzierzbicki@thehutgroup.com>
2023-06-22 20:30:41 +0200

    Reverse fix

--- ditoelement.js
+++ ditoelement.js
@@ -460,6 +460,7 @@ class DitoElement extends HTMLElement {
         }
       }
 
+      // Actioning ifs twice because of possible hidden fors
       this.$self.children.forEach(child => {
         try {
           this.actionIf(child, child.$self?.actions?.ifs || [], 'ifs');
@@ -1022,7 +1023,7 @@ class DitoElement extends HTMLElement {
       const anchor = anchors[i];
 
       if (!document.body.contains(anchor.parentElement)) {
-        if (anchor.nodeName === 'A') {
+        if (anchors.length > 1) {
           node.$self.for.anchors.splice(i, 1);
           i--;
         }
